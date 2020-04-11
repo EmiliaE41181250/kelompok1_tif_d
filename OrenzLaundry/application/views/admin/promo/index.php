5068c2a5a1f2e3a56bd0d2f6b5d2f18c19d0307b
Primasdika Yunia Putra <dickayunia1@gmail.com>
2020-04-11 22:41:31 +0700

    Memperbaiki Bug dan kekurangan CRUD Promo

--- OrenzLaundry/application/views/admin/promo/index.php
+++ OrenzLaundry/application/views/admin/promo/index.php
@@ -1,49 +1,71 @@
-<div class="container-fluid">
+  <div class="container-fluid">
 
-  <div class="block-header">
-      <div class="row">
-          <div class="col-12">
-              <h2 class="font-weight-bolder">Data Promo</h2>
-              <ul class="breadcrumb bg-transparent ml-n3 mt-n3">
-                  <li class="breadcrumb-item"><a href="<?=base_url()?>admin/dashboard"><i class="fa fa-home"></i> OrenzLaundry</a></li>
-                  <li class="breadcrumb-item active">Promo</li>
-              </ul>
-          </div>
+    <div class="block-header">
+        <div class="row">
+            <div class="col-12">
+                <h2 class="font-weight-bolder">Data Promo</h2>
+                <ul class="breadcrumb bg-transparent ml-n3 mt-n4 mb-0">
+                    <li class="breadcrumb-item"><a href="<?=base_url()?>admin/dashboard"><i class="fa fa-home"></i> OrenzLaundry</a></li>
+                    <li class="breadcrumb-item active">Promo</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+      <div class="col-6">
+        <button class="btn btn-sm btn-ijo mb-2" data-toggle="modal" data-target="#tambah_barang"><i class="fas fa-plus fa-sm mr-2"></i>Tambah Barang</button>
       </div>
+      <div class="col-6 text-right">
+        <a class="btn btn-sm btn-warning mb-2" href="<?=base_url()?>admin/promo"><i class="fas fa-file-pdf fa-sm mr-2"></i>Cetak Pdf</a>
+      </div>
+    </div>
+
+    <?php echo $this->session->flashdata('pesan');?>
+    <div class="row card shadow">
+      <div class="col card-body table-responsive">
+        <table class="table table-bordered bg-white" id="dataTable" width="100%" cellspacing="0">
+          <thead>
+            <tr class="font-weight-bolder">
+              <th>NO</th>
+              <th>JUDUL PROMO</th>
+              <th>DISKON</th>
+              <th>AWAL</th>
+              <th>AKHIR</th>
+              <th>STATUS</th>
+              <th>AKSI</th>
+            </tr>
+          </thead>
+
+          <tbody>
+          <?php 
+          $no = 1;
+          foreach ($promo as $prm ) { ?>
+            <tr>
+              <td><?=$no++?></td>
+              <td><?=$prm->judul_promo?></td>
+              <td><?=$prm->jumlah?></td>
+              <td><?=$prm->awal?></td>
+              <td><?=$prm->akhir?></td>
+              <td class="text-center">
+                <?php if($prm->status == "Aktif"){?>
+                <span class="badge badge-pill px-4 badge-warning"><?=$prm->status?></span>
+                <?php }else{ ?>
+                <span class="badge badge-pill px-4 badge-secondary"><?=$prm->status?></span>
+                <?php }?>
+              </td>
+              <td class="text-center">
+                <?php echo anchor('admin/promo/edit/' . $prm->id_promo, '
+                <div class="btn btn-primary btn-sm mr-2"><i class="fa fa-edit"></i></div>')?>
+                <?php echo anchor('admin/promo/destroy/' . $prm->id_promo, '
+                <div class="btn btn-danger btn-sm"><i class="fa fa-trash"></i></div>')?>
+              </td>
+            </tr>
+          <?php }?>
+          </tbody>
+        </table>
+      </div>
+    </div>
   </div>
-  <button class="btn btn-sm btn-ijo mb-3" data-toggle="modal" data-target="#tambah_barang"><i class="fas fa-plus fa-sm mr-2"></i>Tambah Barang</button>
-
-  <?php echo $this->session->flashdata('pesan');?>
-  <table id="DataTable" class="table table-bordered table table-white">
-    <tr class="font-weight-bolder">
-      <th>No</th>
-      <th>JUDUL PROMO</th>
-      <th>DISKON</th>
-      <th>AWAL</th>
-      <th>AKHIR</th>
-      <th>STATUS</th>
-      <th colspan="3">AKSI</th>
-    </tr>
-
-    <?php 
-    $no = 1;
-    foreach ($promo as $prm ) { ?>
-      <tr>
-        <td><?=$no++?></td>
-        <td><?=$prm->judul_promo?></td>
-        <td><?=$prm->jumlah?></td>
-        <td><?=$prm->awal?></td>
-        <td><?=$prm->akhir?></td>
-        <td><?=$prm->status?></td>
-        <td>
-          <?php echo anchor('admin/promo/edit/' . $prm->id_promo, '
-          <div class="btn btn-primary btn-sm mr-2"><i class="fa fa-edit"></i></div>')?>
-          <?php echo anchor('admin/promo/destroy/' . $prm->id_promo, '
-          <div class="btn btn-danger btn-sm"><i class="fa fa-trash"></i></div>')?>
-        </td>
-      </tr>
-    <?php }?>
-  </table>
 </div>
 
 <!-- Modal -->
--- OrenzLaundry/application/views/templates/footer.php
+++ OrenzLaundry/application/views/templates/footer.php
@@ -53,6 +53,11 @@
 
   <!-- Page level plugins -->
   <script src="<?=base_url()?>assets/admin/vendor/chart.js/Chart.min.js"></script>
+  <script src="<?=base_url()?>assets/admin/vendor/datatables/jquery.dataTables.min.js"></script>
+  <script src="<?=base_url()?>assets/admin/vendor/datatables/dataTables.bootstrap4.min.js"></script>
+
+  <!-- Page level custom scripts -->
+  <script src="<?=base_url()?>assets/admin/js/demo/datatables-demo.js"></script>
 
   <!-- Page level custom scripts -->
   <script src="<?=base_url()?>assets/admin/js/demo/chart-area-demo.js"></script>
