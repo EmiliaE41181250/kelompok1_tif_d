11e67dce31b3a5fbea46edb672e732b1f1b9b268
YogaE41181231 <ichayoga1@gmail.com>
2020-04-17 15:11:27 +0700

    detail paket

--- OrenzLaundry/application/views/admin/paket/v_detailpaket.php
+++ OrenzLaundry/application/views/admin/paket/v_detailpaket.php
@@ -4,20 +4,17 @@
     <div class="block-header">
         <div class="row">
             <div class="col-12">
-                <h2 class="font-weight-bolder">Data Paket</h2>
+                <h2 class="font-weight-bolder">Detail Data Paket</h2>
                 <ul class="breadcrumb bg-transparent ml-n3 mt-n4 mb-0">
                     <li class="breadcrumb-item"><a href="<?=base_url()?>admin/dashboard"><i class="fa fa-home"></i> OrenzLaundry</a></li>
-                    <li class="breadcrumb-item active">Paket</li>
+                    <li class="breadcrumb-item active">Detail Paket</li>
                 </ul>
             </div>
         </div>
     </div>
 
     <div class="row">
-      <div class="col-6">
-        <button class="btn btn-sm btn-ijo mb-2" data-toggle="modal" data-target="#tambah_barang"><i class="fas fa-plus fa-sm mr-2"></i>Tambah Barang</button>
-      </div>
-      <div class="col-6 text-right">
+      <div class="col-12 text-right">
         <a class="btn btn-sm btn-warning mb-2" href="<?=base_url()?>admin/C_paket"><i class="fas fa-file-pdf fa-sm mr-2"></i>Cetak Pdf</a>
       </div>
     </div>
