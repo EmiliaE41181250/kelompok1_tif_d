5b82997cc47596dc7e8095f295574e886b1b6f2b
litaafia <55611058+litaafia@users.noreply.github.com>
2020-04-21 20:14:36 +0700

    isi paket ga fix

--- OrenzLaundry/application/controllers/admin/C_isipaket.php
+++ OrenzLaundry/application/controllers/admin/C_isipaket.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class C_paket extends CI_Controller {
+class C_isipaket extends CI_Controller {
 
     function __construct(){
         parent::__construct();	
@@ -15,7 +15,7 @@ class C_paket extends CI_Controller {
             $data['isi_paket'] = $this->m_isipaket->tampil_data()->result();
             $this->load->view('templates/header');
             $this->load->view('templates/sidebar');
-            $this->load->view('admin/paket/v_isipaket', $data);
+            $this->load->view('admin/isipaket/v_isipaket', $data);
             $this->load->view('templates/footer');
         }
 
@@ -24,68 +24,40 @@ class C_paket extends CI_Controller {
         $data['isi_paket'] = $this->m_isipaket->edit()->result();
           $this->load->view('templates/header');
           $this->load->view('templates/sidebar');
-          $this->load->view('admin/paket/v_edit', $data);
+          $this->load->view('admin/isipaket/v_edit_ip', $data);
           $this->load->view('templates/footer');
         }
       
         public function tambah()
         {
           // memeriksa apakah ada id pada database
-          $row_id = $this->m_data_paket->getId()->num_rows();
+          $row_id = $this->m_isipaket->getId()->num_rows();
           // mengambil 1 baris data terakhir
-          $old_id = $this->m_data_paket->getId()->row();
+          $old_id = $this->m_isipaket->getId()->row();
       
           if($row_id>0){
             // melakukan auto number dari id terakhir
-          $id = $this->primslib->autonumber($old_id->id_paket, 3, 12);
+          $id = $this->primslib->autonumber($old_id->id_isi_paket, 3, 12);
           }else{
             // generate id pertama kali jika tidak ada data sama sekali di dalam database
-          $id = 'PKT000000000001';
+          $id = 'IPT000000000001';
           }
       
           $created_by = "admin";
           $created_at = date('Y-m-d H:i:s');
-          $gambar_paket = $_FILES['gambar'];
-          // menjalankan perintah untuk mengupload gambar
-          // if ($_FILES['gambar']['name'] != null) {
-          //   $gambar_promo = $_FILES['gambar']['name'];
-          //   $gambar_promo = $this->primslib->upload_image('gambar', $gambar_promo, 'jpg|jpeg|png', '3024');
-          // }
-
-          if($gambar_paket){
-            $config['allowed_types'] = 'gif|jpg|png';
-            $config['max_size']='2048';
-            $config['upload_path']='./assets/files/gambar_paket/';
-
-            $this->load->library('upload', $config);
-            if ($gambar_paket == '') {
-              $gambar_paket = null;
-            }else {
-              if (!$this->upload->do_upload('gambar')) {
-                $this->session->set_flashdata('message','<div class="alert alert-danger" role="alert">
-                Gagal menambah foto!
-              </div>');
-              }else {
-                $gambar_paket = $this->upload->data('file_name');
-              }
-            }
-          }
       
           // merekam data yang dikirim melalui form
           $data = array(
-            'id_paket' => $id,
-            'nama_paket' => $this->input->post('nama_paket'),
-            'id_jenis_paket' => $this->input->post('nama_jenis_paket'),
-            'id_isi_paket' => $this->input->post('nama_isi_paket'),
-            'harga' => $this->input->post('harga'),
-            'gambar' => $gambar_paket,
-            'id_durasi' => $this->input->post('durasi_paket'),
-            'id_barang' => $this->input->post('nama_barang'),
-            'status' => $this->input->post('status')
+            'id_isi_paket' => $id,
+            'nama_isi_paket' => $this->input->post('nama_isi_paket'),
+            'keterangan' => $this->input->post('keterangan'),
+            'status' => $this->input->post('status'),
+            'created_by' => $created_by,
+            'created_at' => $created_at
           );
       
           // menjalankan fungsi insert pada model_promo untuk menambah data ke database
-          $this->m_data_paket->insert($data, 'paket');
+          $this->m_isipaket->insert($data, 'isi_paket');
           // mengirim pesan berhasil dihapus
           $this->session->set_flashdata('pesan', '
           <div class="alert alert-success alert-dismissible fade show" role="alert">
@@ -96,76 +68,27 @@ class C_paket extends CI_Controller {
           </div>
           ');
           // mengarahkan ke halaman tabel paket
-          redirect('admin/C_paket');
+          redirect('admin/C_isipaket');
         }
 
-        public function detail($id){
-          $this->load->model('m_data_paket');
-          $detail = $this->m_data_paket->detail_data($id);
-          $data['detail'] = $detail;
-          
-          $this->load->view('templates/header');
-          $this->load->view('templates/sidebar');
-          $this->load->view('admin/paket/v_detailpaket', $data);
-          $this->load->view('templates/footer');
-        }
-      
         public function update()
         {
           // merekam id sebagai parameter where saat update
-          $where = array('id_paket' => $this->input->post('id_paket'));
+          $where = array('id_isi_paket' => $this->input->post('id_isi_paket'));
           // menentukan siapa dan kapan baris data ini diperbarui
           $updated_by = "admin";
           $updated_at = date('Y-m-d H:i:s');
-          $gambar_paket = null;
-          $gambar_paket = $_FILES['gambar'];
-          // memeriksa apakah admin mengganti gambar atau tidak
-          if($gambar_paket){
-            $config['allowed_types'] = 'gif|jpg|png';
-            $config['max_size']='2048';
-            $config['upload_path']='./assets/files/gambar_paket/';
-
-            $this->load->library('upload', $config);
-            if ($gambar_paket == '') {
-              $gambar_paket = null;
-            }else {
-              if (!$this->upload->do_upload('gambar')) {
-                $this->session->set_flashdata('message','<div class="alert alert-danger" role="alert">
-                Gagal menambah foto!
-              </div>');
-              }else {
-                $gambar_paket = $this->upload->data('file_name');
-              }
-            }
-          }
-            // jika tidak memilih gambar
-
-          if($gambar_paket){
-            $data = array(
-              'nama_paket' => $this->input->post('nama_paket'),
-              'id_jenis_paket' => $this->input->post('nama_jenis_paket'),
-              'id_isi_paket' => $this->input->post('nama_isi_paket'),
-              'harga' => $this->input->post('harga'),
-              'gambar' => $gambar_paket,
-              'id_durasi' => $this->input->post('durasi_paket'),
-              'id_barang' => $this->input->post('nama_barang'),
-              'status' => $this->input->post('status')
-            );
-          }else{
-            $data = array(
-              'nama_paket' => $this->input->post('nama_paket'),
-              'id_jenis_paket' => $this->input->post('nama_jenis_paket'),
-              'id_isi_paket' => $this->input->post('nama_isi_paket'),
-              'harga' => $this->input->post('harga'),
-              'id_durasi' => $this->input->post('durasi_paket'),
-              'id_barang' => $this->input->post('nama_barang'),
-              'status' => $this->input->post('status')
-            );
-          }
+        // masukkan data yang akan di update
+        $data = array(
+        'nama_isi_paket' => $this->input->post('nama_isi_paket'),
+        'updated_by' => $updated_by,
+        'updated_at' => $updated_at
+        );
+          
           
       
-          // menjalankan method update pada model promo
-          $this->m_data_paket->update($where, $data, 'paket');
+          // menjalankan method update pada model 
+          $this->m_isipaket->update($where, $data, 'isi_paket');
       
           // mengirim pesan berhasil update data
           $this->session->set_flashdata('pesan', '
@@ -177,16 +100,16 @@ class C_paket extends CI_Controller {
           </div>
           ');
           // mengarahkan ke halaman tabel promo
-          redirect('admin/C_paket');
+          redirect('admin/C_isipaket');
         }
       
         // method yang berfungsi menghapus data
         public function destroy($id)
         {
           // deklarasi $where by id
-          $where = array('id_paket' => $id);
+          $where = array('id_isi_paket' => $id);
           // menjalankan fungsi delete pada model_promo
-          $this->m_data_paket->delete($where, 'paket');
+          $this->m_isipaket->delete($where, 'isi_paket');
           // mengirim pesan berhasil dihapus
           $this->session->set_flashdata('pesan', '
           <div class="alert alert-danger alert-dismissible fade show" role="alert">
@@ -197,7 +120,7 @@ class C_paket extends CI_Controller {
           </div>
           ');
           // mengarahkan ke halaman tabel promo
-          redirect('admin/C_paket');
+          redirect('admin/C_isipaket');
         }
 
         
--- OrenzLaundry/application/views/admin/isipaket/v_isipaket.php
+++ OrenzLaundry/application/views/admin/isipaket/v_isipaket.php
@@ -16,7 +16,7 @@
 
     <div class="row">
         <div class="col-6">
-            <button class="btn btn-sm btn-ijo mb-2" data-toggle="modal" data-target="#tambah_barang"><i class="fas fa-plus fa-sm mr-2"></i>Tambah Isi Paket</button>
+            <button class="btn btn-sm btn-ijo mb-2" data-toggle="modal" data-target="#tambah_isipaket"><i class="fas fa-plus fa-sm mr-2"></i>Tambah Isi Paket</button>
         </div>
         <div class="col-6 text-right">
             <a class="btn btn-sm btn-warning mb-2" href="<?= base_url() ?>admin/v_isipaket"><i class="fas fa-file-pdf fa-sm mr-2"></i>Cetak Pdf</a>
@@ -29,9 +29,11 @@
             <table class="table table-bordered bg-white" id="dataTable" width="100%" cellspacing="0">
                 <thead>
                     <tr>
-                        <th>No</th>
-                        <th>Isi Paket</th>
-                        <th>Aksi</th>
+                        <th>NO</th>
+                        <th>ISI PAKET</th>
+                        <th>KETERANGAN</th>
+                        <th>STATUS</th>
+                        <th>AKSI</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -41,6 +43,8 @@
                         <tr>
                             <td><?= $no++ ?></td>
                             <td><?= $ip->nama_isi_paket ?></td>
+                            <td><?= $ip->keterangan ?></td>
+                            <td><?= $ip->status ?></td>
                             <td class="text-center">
                                 <?php if ($ip->status == "Aktif") { ?>
                                     <span class="badge badge-pill px-4 badge-warning"><?= $ip->status ?></span>
@@ -49,13 +53,12 @@
                                 <?php } ?>
                             </td>
                             <td class="text-center">
-                                <?php echo anchor('admin/C_isipaket/' . $ip->id_isi_paket, '
-                <div class="btn btn-info btn-sm mr-2 pr-3 pl-3"><i class="fa fa-info"></i></div>') ?>
-                                <?php echo anchor('admin/C_isipaket/edit/' . $ip->id_isi_paket, '
+                            <?php echo anchor('admin/C_isipaket/edit/' . $ip->id_isi_paket, '
                 <div class="btn btn-primary btn-sm mr-2"><i class="fa fa-edit"></i></div>') ?>
                                 <?php echo anchor('admin/C_isipaket/destroy/' . $ip->id_isi_paket, '
                 <div class="btn btn-danger btn-sm"><i class="fa fa-trash"></i></div>') ?>
-
+                                
+                               
                             </td>
                         </tr>
                     <?php } ?>
@@ -77,9 +80,24 @@
                 </div>
                 <div class="modal-body">
                     <div class="form-group">
-                        <label for="nama_isi_paket">Isi Paket</label>
-                        <input type="text" name="nama_jenispaket" id="nama_jenispaket" class="form-control" placeholder="Masukkan Nama Jenis Paket . ." aria-describedby="namajenispaket" maxlength="100">
-                        <small id="namapaket" class="text-muted">Masukkan Nama jenis Paket tidak lebih dari 100 Karakter</small>
+                        <label for="nama_isi_paket">Nama Isi Paket</label>
+                        <input type="text" name="nama_isi_paket" id="nama_isi_paket" class="form-control" placeholder="Masukkan Isi Paket . ." aria-describedby="nama_isi_paket" maxlength="100">
+                        <small id="nama_isi_paket" class="text-muted">Masukkan Isi Paket tidak lebih dari 100 Karakter</small>
+                    </div>
+                    <div class="modal-body">
+                    <div class="form-group">
+                        <label for="nama_isi_paket">Keterangan</label>
+                        <input type="text" name="keterangan" id="keterangan" class="form-control" placeholder="Tambahkan Keterangan . ." aria-describedby="nama_isi_paket" maxlength="100">
+                        <small id="nama_isi_paket" class="text-muted">Tambahkan Keterangan Isi Paket</small>
+                    </div>
+                    <div class="form-group w-50">
+                        <label for="status">Status</label>
+                         <select class="form-control" name="status" id="status">
+                            <option>Pilih Status Isi Paket :</option>
+                            <option value="Aktif">Aktif</option>
+                             <option value="Draft">Draft</option>
+                        </select>
+                    </div>
                     </div>
 
                     <div class="modal-footer">
