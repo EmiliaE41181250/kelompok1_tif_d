c607f1027ea5a8bc8791d9eb1589087c27d62bf5
litaafia <55611058+litaafia@users.noreply.github.com>
2020-04-26 20:53:52 +0700

    login
    lupa push

--- OrenzLaundry/application/views/admin/login.php
+++ OrenzLaundry/application/views/admin/login.php
@@ -1,94 +1,80 @@
+
 <!DOCTYPE html>
-<html lang="en">
-
+<html>
 <head>
-
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta name="description" content="">
-  <meta name="author" content="">
-
-  <title>Login</title>
-
-  <!-- Custom fonts for this template-->
-  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-  <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
-
-  <!-- Custom styles for this template-->
-  <link href="css/sb-admin-2.min.css" rel="stylesheet">
-
+	<title>Membuat Login Dengan CodeIgniter | www.malasngoding.com</title>
 </head>
+<style>
+	body{
+	font-family: sans-serif;
+	background: #ffffff;
+}
 
-<body class="bg-gradient-primary">
+h1{
+	text-align: center;
+	/*ketebalan font*/
+	font-weight: 300;
+}
 
-  <div class="container">
+.tulisan_login{
+	text-align: center;
+	/*membuat semua huruf menjadi kapital*/
+	text-transform: uppercase;
+}
 
-    <!-- Outer Row -->
-    <div class="row justify-content-center">
+.kotak_login{
+	width: 350px;
+	background: #90cade;
+	/*meletakkan form ke tengah*/
+	margin: 80px auto;
+	padding: 30px 20px;
+}
 
-      <div class="col-xl-10 col-lg-12 col-md-9">
+label{
+	font-size: 11pt;
+}
 
-        <div class="card o-hidden border-0 shadow-lg my-5">
-          <div class="card-body p-0">
-            <!-- Nested Row within Card Body -->
-            <div class="row">
-              <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
-              <div class="col-lg-6">
-                <div class="p-5">
-                  <div class="text-center">
-                    <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
-                  </div>
-                  <form class="user">
-                    <div class="form-group">
-                      <input type="email" class="form-control form-control-user" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Enter Email Address...">
-                    </div>
-                    <div class="form-group">
-                      <input type="password" class="form-control form-control-user" id="exampleInputPassword" placeholder="Password">
-                    </div>
-                    <div class="form-group">
-                      <div class="custom-control custom-checkbox small">
-                        <input type="checkbox" class="custom-control-input" id="customCheck">
-                        <label class="custom-control-label" for="customCheck">Remember Me</label>
-                      </div>
-                    </div>
-                    <a href="index.html" class="btn btn-primary btn-user btn-block">
-                      Login
-                    </a>
-                    <hr>
-                    <a href="index.html" class="btn btn-google btn-user btn-block">
-                      <i class="fab fa-google fa-fw"></i> Login with Google
-                    </a>
-                    <a href="index.html" class="btn btn-facebook btn-user btn-block">
-                      <i class="fab fa-facebook-f fa-fw"></i> Login with Facebook
-                    </a>
-                  </form>
-                  <hr>
-                  <div class="text-center">
-                    <a class="small" href="forgot-password.html">Forgot Password?</a>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+.form_login{
+	/*membuat lebar form penuh*/
+	box-sizing : border-box;
+	width: 100%;
+	padding: 10px;
+	font-size: 11pt;
+	margin-bottom: 20px;
+}
 
-      </div>
+.tombol_login{
+	background: #d1e1e8;
+	color: black;
+	font-size: 11pt;
+	width: 100%;
+	border: none;
+	border-radius: 3px;
+	padding: 10px 20px;
+}
 
-    </div>
-
-  </div>
-
-  <!-- Bootstrap core JavaScript-->
-  <script src="vendor/jquery/jquery.min.js"></script>
-  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-
-  <!-- Core plugin JavaScript-->
-  <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
-
-  <!-- Custom scripts for all pages-->
-  <script src="js/sb-admin-2.min.js"></script>
-
+.link{
+	color: #232323;
+	text-decoration: none;
+	font-size: 10pt;
+}
+</style>
+<body>
+	
+	<!-- aksi form ini diarah ke fungsi aksi_login yang terdapat di controler login -->
+	<div class="kotak_login">
+	<form action="<?php echo base_url('index.php/admin/login/aksi_login'); ?>" method="post">		
+		<label>Username</label>
+		<input type="text" name="username" class="form_login" placeholder="Username atau email ..">
+ 
+		<label>Password</label>
+		<input type="password" name="password" class="form_login" placeholder="Password ..">
+ 
+		<input type="submit" class="tombol_login" value="LOGIN">
+ 
+		<br/>
+		<br/>
+	</form>
+	</div>
 </body>
-
 </html>
