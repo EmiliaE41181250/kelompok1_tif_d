2a5b7035fb481e953f1cd28adf7bb6744f7e0522
litaafia <55611058+litaafia@users.noreply.github.com>
2020-04-27 07:50:46 +0700

    update
    login

--- OrenzLaundry/application/views/admin/login.php
+++ OrenzLaundry/application/views/admin/login.php
@@ -1,68 +1,103 @@
+<style>
+	/* Coded with love by Mutiullah Samim */
+	body,
+		html {
+			margin: 0;
+			padding: 0;
+			height: 100%;
+			background-color: #5dcf40 !important;
+			background-image: linear-gradient(to bottom right, #fcbf30 , #5dcf40);
+		}
+		.user_card {
+			height: 500px;
+			width: 350px;
+			margin-top: auto;
+			margin-bottom: auto;
+			background: #ffffff;
+			position: relative;
+			display: flex;
+			justify-content: center;
+			flex-direction: column;
+			padding: 10px;
+			box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+			-webkit-box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+			-moz-box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+			border-radius: 5px;
+
+		}
+		.brand_logo_container {
+			position: absolute;
+			height: 170px;
+			width: 170px;
+			top: -75px;
+			border-radius: 50%;
+			background: #60a3bc;
+			padding: 10px;
+			text-align: center;
+		}
+		.brand_logo {
+			height: 150px;
+			width: 150px;
+			border-radius: 50%;
+			border: 2px solid white;
+		}
+		.form_container {
+			margin-top: 100px;
+		}
+		.login_btn {
+			width: 100%;
+			background: #fcbf30 !important;
+			color: white !important;
+		}
+		.login_btn:focus {
+			box-shadow: none !important;
+			outline: 0px !important;
+		}
+		.login_container {
+			padding: 0 2rem;
+		}
+		.input-group-text {
+			background: #fcbf30 !important;
+			color: white !important;
+			border: 0 !important;
+			border-radius: 0.25rem 0 0 0.25rem !important;
+		}
+		.input_user,
+		.input_pass:focus {
+			box-shadow: none !important;
+			outline: 0px !important;
+		}
+		.custom-checkbox .custom-control-input:checked~.custom-control-label::before {
+			background-color: #c0392b !important;
+		}
+</style>
+
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<!------ Include the above in your HEAD tag ---------->
 
 <!DOCTYPE html>
 <html>
+    
 <head>
-	<title>Membuat Login Dengan CodeIgniter | www.malasngoding.com</title>
+	<title>Login</title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" integrity="sha384-gfdkjb5BdAXd+lj+gudLWI+BXq4IuLW5IT+brZEZsLFm++aCMlF1V92rMkPaX4PP" crossorigin="anonymous">
 </head>
-<style>
-	body{
-	font-family: sans-serif;
-	background: #ffffff;
-}
-
-h1{
-	text-align: center;
-	/*ketebalan font*/
-	font-weight: 300;
-}
-
-.tulisan_login{
-	text-align: center;
-	/*membuat semua huruf menjadi kapital*/
-	text-transform: uppercase;
-}
-
-.kotak_login{
-	width: 350px;
-	background: #90cade;
-	/*meletakkan form ke tengah*/
-	margin: 80px auto;
-	padding: 30px 20px;
-}
-
-label{
-	font-size: 11pt;
-}
-
-.form_login{
-	/*membuat lebar form penuh*/
-	box-sizing : border-box;
-	width: 100%;
-	padding: 10px;
-	font-size: 11pt;
-	margin-bottom: 20px;
-}
-
-.tombol_login{
-	background: #d1e1e8;
-	color: black;
-	font-size: 11pt;
-	width: 100%;
-	border: none;
-	border-radius: 3px;
-	padding: 10px 20px;
-}
-
-.link{
-	color: #232323;
-	text-decoration: none;
-	font-size: 10pt;
-}
-</style>
+<!--Coded with love by Mutiullah Samim-->
 <body>
-	
-	<!-- aksi form ini diarah ke fungsi aksi_login yang terdapat di controler login -->
-	<div class="kotak_login">
+	<div class="container h-100">
+		<div class="d-flex justify-content-center h-100">
+			<div class="user_card">
+				<div class="d-flex justify-content-center">
+					<div class="brand_logo_container">
+						<img src="assets/admin/img/LaundryRegis.png" class="brand_logo" alt="Logo">
+					</div>
+				</div>
+				<div class="d-flex justify-content-center form_container">
+				<!-- <div class="kotak_login">
 	<form action="<?php echo base_url('index.php/admin/login/aksi_login'); ?>" method="post">		
 		<label>Username</label>
 		<input type="text" name="username" class="form_login" placeholder="Username atau email ..">
@@ -74,7 +109,37 @@ label{
  
 		<br/>
 		<br/>
-	</form>
+	</form> -->
+	<form action="<?php echo base_url('index.php/admin/login/aksi_login'); ?>" method="post">
+						<div class="input-group mb-3">
+							<div class="input-group-append">
+								<span class="input-group-text"><i class="fas fa-user"></i></span>
+							</div>
+							<input type="text" name="username" class="form-control input_user" value="" placeholder="username">
+						</div>
+						<div class="input-group mb-2">
+							<div class="input-group-append">
+								<span class="input-group-text"><i class="fas fa-key"></i></span>
+							</div>
+							<input type="password" name="password" class="form-control input_pass" value="" placeholder="password">
+						</div>
+						<div class="form-group">
+							<div class="custom-control custom-checkbox">
+								<input type="checkbox" class="custom-control-input" id="customControlInline">
+								<label class="custom-control-label" for="customControlInline">Remember me</label>
+							</div>
+						</div>
+							<div class="d-flex justify-content-center mt-3 login_container">
+				 	<input type="submit" name="button" class="btn login_btn" value=LOGIN>
+				   </div>
+					</form>
+				</div>
+					<div class="d-flex justify-content-center links">
+						<a href="#">Forgot your password?</a>
+					</div>
+				</div>
+			</div>
+		</div>
 	</div>
 </body>
 </html>
