8928d52dd62088a3fd80aff03689dbc21b246267
rahmanabilla <55685816+rahmanabilla@users.noreply.github.com>
2020-05-13 15:47:07 +0700

    edit dan durasi paket

---
+++ OrenzLaundry/application/views/admin/detail.php
@@ -0,0 +1,64 @@
+<div class="container-fluid">
+  <div class="row justify-content-center py-3">
+    <div class="col-md-8 card p-0">
+      <div class="card-header pb-0">
+        <h2 class="font-weight-bolder mb-0">Detail Durasi Paket</h2>
+        <ul class="breadcrumb bg-transparent ml-n3 mt-n3 mb-0">
+            <li class="breadcrumb-item"><a href="<?=base_url()?>admin/dashboard"><i class="fa fa-home"></i> OrenzLaundry</a></li>
+            <li class="breadcrumb-item"><a href="<?=base_url()?>admin/C_isipaket"></i> Durasi Paket</a></li>
+            <li class="breadcrumb-item active">Detail Durasi Paket</li>
+        </ul>
+      </div>
+      <div class="card-body">
+
+        <?php foreach ($durasi_paket as $ip) { ?>
+          
+          <table class="table table-borderless">
+            <thead>
+              <tr>
+                <th>ID Durasi Paket</th>
+                <th>:</th>
+                <th><?=$ip->id_durasi_paket?></th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Durasi</td>
+                <td>:</td>
+                <td><?=$ip->Durasi?></td>
+              </tr>
+              <tr>
+                <td>Keterangan</td>
+                <td>:</td>
+                <td class="text-justify"><?=$ip->keterangan?></td>
+              </tr>
+                <td>Status</td>
+                <td>:</td>
+                <td>
+                  <?php if($ip->status == "Aktif"){?>
+                  <span class="badge badge-pill px-4 badge-warning"><?=$ip->status?></span>
+                  <?php }else{ ?>
+                  <span class="badge badge-pill px-4 badge-secondary"><?=$ip->status?></span>
+                  <?php }?>
+                </td>
+              </tr>
+              <tr>
+                <td>Dibuat Oleh</td>
+                <td>:</td>
+                <td><span class="font-weight-bolder"><?=$ip->created_by?></span>, pada <span class="font-italic text-success"><?=$ip->created_at?></span></td>
+              </tr>
+              <tr>
+                <td>Terakhir diubah oleh</td>
+                <td>:</td>
+                <td><span class="font-weight-bolder"><?=$ip->updated_by?></span>, pada <span class="font-italic text-warning"><?=$ip->updated_at?></span></td>
+              </tr>
+            </tbody>
+          </table>
+
+      </div>
+    </div>
+  </div>
+
+  
+</div>
+<?php }?>
