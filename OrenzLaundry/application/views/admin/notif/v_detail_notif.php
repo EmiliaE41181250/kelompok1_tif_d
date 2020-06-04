876edb82929c6fcf7bb2b2fd0f442519d492e1c2
YogaE41181231 <ichayoga1@gmail.com>
2020-06-04 17:52:18 +0700

    detail notif

---
+++ OrenzLaundry/application/views/admin/notif/v_detail_notif.php
@@ -0,0 +1,21 @@
+<div class="container-fluid">
+
+<!-- Page Heading -->
+    <div class="block-header">
+        <div class="row">
+            <div class="col-12">
+                <h2 class="font-weight-bolder">Detail Data Paket</h2>
+                <ul class="breadcrumb bg-transparent ml-n3 mt-n4 mb-0">
+                    <li class="breadcrumb-item"><a href="<?=base_url()?>admin/dashboard"><i class="fa fa-home"></i> OrenzLaundry</a></li>
+                    <li class="breadcrumb-item active">Detail Paket</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+      <div class="col-12 text-right">
+        <a class="btn btn-sm btn-warning mb-2" href="<?=base_url()?>admin/C_paket"><i class="fas fa-file-pdf fa-sm mr-2"></i>Cetak Pdf</a>
+      </div>
+    </div>
+    
