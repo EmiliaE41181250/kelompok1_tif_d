5662e734b28d6b170922a98bc7a13fc79d13ee75
YogaE41181231 <ichayoga1@gmail.com>
2020-06-15 18:22:22 +0700

    tampilan detail notif

--- OrenzLaundry/application/views/admin/notif/v_detail_notif.php
+++ OrenzLaundry/application/views/admin/notif/v_detail_notif.php
@@ -19,3 +19,73 @@
       </div>
     </div>
     
+    <div class="container-fluid">
+  <form action="transaksi_query.php" method="post">
+  <div class="card m-5 shadow">
+    <div class="card-header text-center text-light bg-primary">
+      <h3 class="d-inline">Pesanan Anda</h3>
+      <a class="mt-2 mr-2 btn btn-light float-right ml-auto" href="nota/nota_pesanan.php?id_pesanan=<?=$id_pesanan?>"><i class="fas fa-fw fa-print"></i></a>
+    </div>
+    <div class="card-body py-4 px-5 ">
+      <table class="w-100 table text-center py-0 m-0">
+        <tbody>
+          <tr>
+            <td class="text-left"><strong>ID PESANAN : </strong><?=$id_pesanan?></td>
+            <td colspan="3" class="text-right"><?=$tgl_psn?></td>
+          <tr>
+        </tbody>
+      </table>
+      <table class="w-100 table table-bordered table-hover text-center">
+        <thead>
+          <tr>
+            <th>Daftar Produk</th>
+            <th>Desain</th>
+            <th>Ket Pembayaran</th>
+            <th>Jumlah Produk</th>
+            <th>Sub Total</th>
+          </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <!-- NAMA PRODUK, WARNA, UKURAN, BAHAN -->
+            <td style="width: 30%;"><p><?php echo "$nama_produk / $jenis_warna / $nama_bahan / $jenis_ukuran";?></p></td>
+            <td><?= $status_desain?><br><?=$upload_desain?></td>
+            <td><?= $ket_pembayaran?></td>
+            <td><?= number_format($quantity, 0,".",".")?></td>
+            <td>Rp. <?=number_format($sub_total, 0,".",".")?>,-</td>
+          </tr>
+          <?php }?>
+          <tr class="font-weight-bolder">
+            <td class="border-0"> </td>
+            <td class="text-right ">Total Harga : </td>
+            <td colspan="3" class="text-right " >Rp. <?=number_format($total_harga, 0,".",".")?>,-</td>
+          <tr>
+          <tr class="font-weight-bolder">
+            <td class="border-0"> </td>
+            <td class="text-right ">Status Pesanan : </td>
+            <td colspan="3" class="text-right " ><?=$ket_status?></td>
+          <tr>
+          <tr class="font-weight-bolder">
+            <td class="border-0"> </td>
+            <td class="text-right ">Lama Pengerjaan : </td>
+            <td colspan="3" class="text-right " ><?=$antrian?> Jam</td>
+          <tr>
+          <tr class="font-weight-bolder">
+            <td class="border-0"> </td>
+            <td class="text-right ">Bukti TF : </td>
+            <td colspan="3" class="text-right " >
+            <?php 
+            if($bukti_tf != null){ ?>
+            <img src="pictures/bukti_transfer/<?=$bukti_tf?>" alt="" class="img-fluid">
+            <?php }else{ 
+            echo "<a href='verif_pembayaran.php?id_pesanan=$id_pesanan&id_bank=$id_bank' class='btn btn-primary px-2'>Upload Bukti TF</a>";
+            } ?>
+            </td>
+          <tr>
+        </tbody>
+      </table>
+      </div>
+    </div>
+  </div>
+</div>
+
