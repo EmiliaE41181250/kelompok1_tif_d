021e27f7dd480c57a9152549f8eee989672ecb6e
Muhammad Wildan Aulia Kahfi <55533786+WildanE41181398@users.noreply.github.com>
2020-06-27 23:56:20 +0700

    Up Dashboard

--- OrenzLaundry/application/views/admin/dashboard.php
+++ OrenzLaundry/application/views/admin/dashboard.php
@@ -17,7 +17,7 @@
                   <div class="row no-gutters align-items-center">
                     <div class="col mr-2">
                       <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Pendapatan per Bulan</div>
-                      <div class="h5 mb-0 font-weight-bold text-gray-800">Rp.</div>
+                      <div class="h5 mb-0 font-weight-bold text-gray-800"> Rp. </div>
                     </div>
                     <div class="col-auto">
                       <i class="fas fa-calendar fa-2x text-gray-300"></i>
@@ -53,13 +53,15 @@
                       <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Pelanggan</div>
                       <div class="row no-gutters align-items-center">
                         <div class="col-auto">
-                          <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800"></div>
+                          <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800"><?= $pelanggan; ?></div>
                         </div>
                         
                       </div>
                     </div>
                     <div class="col-auto">
-                      <i class="fas fa-clipboard-list fa-2x text-gray-300"></i>
+                      <a href="<?= base_url('admin/user');?>">
+                        <i class="fas fa-clipboard-list fa-2x text-gray-300"></i>
+                      </a>
                     </div>
                   </div>
                 </div>
@@ -73,7 +75,7 @@
                   <div class="row no-gutters align-items-center">
                     <div class="col mr-2">
                       <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">History Transaksi</div>
-                      <div class="h5 mb-0 font-weight-bold text-gray-800"></div>
+                      <div class="h5 mb-0 font-weight-bold text-gray-800"> <?= $history; ?></div>
                     </div>
                     <div class="col-auto">
                       <i class="fas fa-comments fa-2x text-gray-300"></i>
