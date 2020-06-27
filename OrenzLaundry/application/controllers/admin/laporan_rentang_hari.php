ef5431487726dd0a635c8d740594be5b940733ed
emilia <emilialaily12@gmail.com>
2020-06-27 08:39:59 +0700

    update laporan tahunan

--- OrenzLaundry/application/controllers/admin/laporan_rentang_hari.php
+++ OrenzLaundry/application/controllers/admin/laporan_rentang_hari.php
@@ -20,14 +20,6 @@ class laporan_rentang_hari extends CI_Controller
         if ($this->input->post('bulanan') != '') {
 
             $hasil_cari = $this->input->post('bulanan');
-
-            /* BUAT VIEW DULUU LEWAT SQL PHPMYADMIN DI DATABASE ORENZLAUNDRY
-            CREATE VIEW vTotalBerat AS
-            SELECT SUM(detail_transaksi.berat) as total_berat, detail_transaksi.id_transaksi
-            FROM detail_transaksi
-            GROUP BY detail_transaksi.id_transaksi
-            */
-
             $data['data_bulanan'] = $this->db->query("SELECT transaksi.tgl_transaksi, SUM(transaksi.total_harga) as total_harga, SUM(vtotalberat.total_berat) as total_berat 
             FROM transaksi, vtotalberat 
             WHERE transaksi.id_transaksi = vtotalberat.id_transaksi AND
