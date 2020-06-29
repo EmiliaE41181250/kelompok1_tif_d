bfe8e68d0d0ca5f2e2a4ca02b934aa5c8f3ef2f3
Muhammad Wildan Aulia Kahfi <55533786+WildanE41181398@users.noreply.github.com>
2020-06-29 13:09:25 +0700

    Dashboard done

--- OrenzLaundry/application/controllers/admin/Dashboard.php
+++ OrenzLaundry/application/controllers/admin/Dashboard.php
@@ -16,6 +16,10 @@ class Dashboard extends CI_Controller
   {
     $data['pelanggan'] = $this->m_dashboard->getAll();
     $data['history'] = $this->m_dashboard->getHistory();
+    $data['bulanan']= $this->m_dashboard->getBulanan()->row()->total_harga;
+    $data['tahunan']= $this->m_dashboard->getTahunan()->row()->total_harga;
+    $data['favorit']= $this->m_dashboard->getFavorit()->result();
+    $data['grafik']= $this->m_dashboard->getMingguan()->result();
     $this->load->view('templates/header');
     $this->load->view('templates/sidebar');
     $this->load->view('admin/dashboard', $data);
