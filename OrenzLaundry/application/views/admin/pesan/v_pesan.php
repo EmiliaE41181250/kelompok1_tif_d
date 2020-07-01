5af190a7bfbfbf6e68cbe16a94707cf0cebfc71b
litaafia <55611058+litaafia@users.noreply.github.com>
2020-07-01 10:59:05 +0700

    Update v_pesan.php
    pesan

---
+++ OrenzLaundry/application/views/admin/pesan/v_pesan.php
@@ -0,0 +1,108 @@
+<!-- Begin Page Content -->
+<div class="container-fluid">
+
+    <!-- Page Heading -->
+    <div class="block-header">
+        <div class="row">
+            <div class="col-12">
+                <h2 class="font-weight-bolder">Data Isi Paket</h2>
+                <ul class="breadcrumb bg-transparent ml-n3 mt-n4 mb-0">
+                    <li class="breadcrumb-item"><a href="<?= base_url() ?>admin/dashboard"><i class="fa fa-home"></i> OrenzLaundry</a></li>
+                    <li class="breadcrumb-item active">Isi Paket</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-6">
+            <button class="btn btn-sm btn-ijo mb-2" data-toggle="modal" data-target="#tambah_barang"><i class="fas fa-plus fa-sm mr-2"></i>Tambah Isi Paket</button>
+        </div>
+    </div>
+
+    <?php echo $this->session->flashdata('pesan'); ?>
+    <div class="row card shadow">
+        <div class="col card-body table-responsive">
+            <table class="table table-bordered bg-white" id="dataTable" width="100%" cellspacing="0">
+                <thead>
+                    <tr>
+                        <th>No</th>
+                        <th>Nama Customer</th>
+                        <th>Subjek Pesan</th>
+                        <th>Aksi</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                    $no = 1;
+                    foreach ($pesan as $psn) { ?>
+                        <tr>
+                            <td><?= $no++ ?></td>
+                            <td><?php foreach ($user as $us){ echo $us->id_user == $psn->id_user ? $us->nama_user : ""; }?></td>
+                            <td><?=$psn->subjek_pesan?></td>
+                            <td class="text-center">
+                            <a href="<?=base_url('admin/C_pesan/detail/' . $psn->id_pesan)?>" 
+                            class="btn btn-info mb-2 btn-sm"><i class="px-1 fa fa-info"></i></a>
+                            <a onclick="return confirm('Apakah anda yakin ingin menghapus item ini ?');" href="<?=base_url('admin/C_pesan/destroy/' . $psn->id_ipesan)?>" 
+                            class="btn btn-danger btn-sm"><i class="fa fa-trash"></i></a>
+
+                            </td>
+                        </tr>
+                    <?php } ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+
+<div class="modal fade" id="tambah_barang" tabindex="-1" role="dialog" aria-labelledby="modelTitleId" aria-hidden="true">
+    <div class="modal-dialog modal-lg" role="document">
+        <div class="modal-content">
+            <form action="<?= base_url() . 'admin/C_isipaket/tambah'; ?>" method="post" enctype="multipart/form-data">
+                <div class="modal-header">
+                    <h5 class="modal-title font-weight-bolder text-ijo">Tambah Data Isi Paket</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <div class="form-group">
+                        <label for="nama_isi_paket">Isi Paket</label>
+                        <input required type="text" name="nama_isi_paket" id="nama_isi_paket" class="form-control" placeholder="Masukkan Nama Isi Paket . ." aria-describedby="nama_isi_paket" maxlength="100">
+                        <small id="nama_isi_paket" class="text-muted">Masukkan Nama Isi Paket tidak lebih dari 100 Karakter</small>
+                    </div>
+                    <div class="modal-body">
+                    <div class="form-group">
+                        <label for="keterangan">Keterangan</label>
+                        <input required type="text" name="keterangan" id="keterangan" class="form-control" placeholder="Masukkan Keterangan . ." aria-describedby="keterangan" maxlength="100">
+                        <small id="keterangan" class="text-muted">Masukkan Keterangan Isi Paket</small>
+                    </div>
+                    <div class="form-group w-50">
+                        <label for="status">Status</label>
+                        <select required class="form-control" name="status" id="status">
+                        <option value="">Pilih Status Isi Paket:</option>
+                        <option value="Aktif">Aktif</option>
+                        <option value="Draft">Draft</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button type="submit" class="btn btn-ijo">Tambah</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+<!-- /.container-fluid -->
+
+</footer>
+<!-- End of Footer -->
+
+</div>
+<!-- End of Content Wrapper -->
+
+
+<!-- End of Page Wrapper -->
+
+
